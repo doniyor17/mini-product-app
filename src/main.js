4e48f4760e643951c070abaaadf3b3f4d922ed74
doniyor17 <doniyor17ibragimov@gmail.com>
2023-12-02 10:30:18 +0500

    Added router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import store from "./store";
+import router from "./router";
 
 const app = createApp(App);
 
-app.use(store).mount("#app");
+app.use(store).use(router).mount("#app");
